22eb0474f41a4ee2b702d93650a0d96a3ad69d86
Divsjains6021 <divsjains6021@gmail.com>
2020-05-10 00:31:14 +0530

    Property Strainer Service

---
+++ src/main/java/com/propertydekho/strainerservice/StrainerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.propertydekho.strainerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrainerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrainerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/propertydekho/strainerservice/StrainerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.propertydekho.strainerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrainerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
