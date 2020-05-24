ca694927a2e519656d63e44bc7819b1d8303ab96
sameer.jain <sameer.jain@flipkart.com>
2020-05-24 22:00:21 +0530

    Resolving merge conflict

--- src/main/java/com/propertydekho/strainerservice/models/PropsFilterInput.java
+++ src/main/java/com/propertydekho/strainerservice/models/PropsFilterInput.java
@@ -1,6 +1,7 @@
 package com.propertydekho.strainerservice.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.propertydekho.strainerservice.filters.PropFilter;
 import lombok.Builder;
 import lombok.Data;
 
